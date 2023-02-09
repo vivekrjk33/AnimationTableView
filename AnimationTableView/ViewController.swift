1683e338a5c194f45e5fe124556bae0589e602a7
vivek <vivekrjk33@gmail.com>
2023-02-09 13:37:04 +0530

    Initial Commit

---
+++ AnimationTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationTableView
+//
+//  Created by Vivek on 09/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
